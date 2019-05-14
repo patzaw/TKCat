9e91cb44ddb88622fcf9ea8dae72db05ea7be074
Patrice Godard <patrice.godard@ucb.com>
2019-05-14 11:53:35 +0200

    Control testing, uploading and overwritting of the data resources

--- tests/Feed-chTKCat.R
+++ tests/Feed-chTKCat.R
@@ -5,6 +5,34 @@ library(TKCat)
 dsr <- "~/Shared/Data-Science/Data-Source-Model-Repository"
 tkcon <- chTKCat()
 
+test <- NA
+while(is.na(test)){
+   test <- readline("Test only? TRUE (default) or FALSE: ")
+   if(test==""){
+      test <- "TRUE"
+   }
+   if(toupper(test) %in% c("TRUE", "FALSE")){
+      test <- as.logical(test)
+   }else{
+      test <- NA
+   }
+}
+
+overwrite <- NA
+while(is.na(overwrite)){
+   overwrite <- readline(
+      "Overwrite existing ClickHouse data? TRUE or FALSE (default): "
+   )
+   if(overwrite==""){
+      overwrite <- "FALSE"
+   }
+   if(toupper(overwrite) %in% c("TRUE", "FALSE")){
+      overwrite <- as.logical(overwrite)
+   }else{
+      overwrite <- NA
+   }
+}
+
 resources <- c(
    "HPO",
    "Reactome",
@@ -19,12 +47,9 @@ resources <- c(
    "PubMed",
    NULL
 )
-for(r in resources){
-   et <- listMDBs(tkcon)
-   if(r %in% et$name){
-      message(glue("{r} is already in the chTKCat"))
-   }else{
-      message(glue("Importing {r}"))
+if(test){
+   for(r in resources){
+      message(glue("Testing {r}"))
       colMbFiles <- list.files(
          file.path(dsr, r, "model/Collections"),
          pattern="[.]json$", ignore.case=TRUE,
@@ -41,9 +66,37 @@ for(r in resources){
          directory=file.path(dsr, r, "data")
       )
       collectionMembers(toload) <- colMb
-      loaded <- TKCat:::addMDBToChTKCat(tkcon, toload)
       rm(toload)
       gc()
+   }
+}else{
+   for(r in resources){
+      et <- listMDBs(tkcon)
+      if(r %in% et$name & !overwrite){
+         message(glue("{r} is already in the chTKCat"))
+      }else{
+         message(glue("Importing {r}"))
+         colMbFiles <- list.files(
+            file.path(dsr, r, "model/Collections"),
+            pattern="[.]json$", ignore.case=TRUE,
+            full.names=TRUE
+         )
+         colMb <- NULL
+         for(f in colMbFiles){
+            colMb <- colMb %>%
+               bind_rows(readCollectionMembers(f))
+         }
+         toload <- readInternalMDB(
+            dataModel=readSQLDataModel(file.path(dsr, r, glue("model/{r}.sql"))),
+            descriptionFile=file.path(dsr, r, "DESCRIPTION.json"),
+            directory=file.path(dsr, r, "data")
+         )
+         collectionMembers(toload) <- colMb
+         TKCat:::rmMDBFromChTKCat(tkcon, r)
+         loaded <- TKCat:::addMDBToChTKCat(tkcon, toload)
+         rm(toload)
+         gc()
+      }
    }
 }
 
