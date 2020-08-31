95d29ca0560c7c131ca919334db1fb0397e88156
Patrice Godard <patrice.godard@ucb.com>
2020-08-31 08:29:29 +0200

    Updated example

--- inst/examples/metaMDB-examples.R
+++ inst/examples/metaMDB-examples.R
@@ -13,24 +13,25 @@ cvpm <- merge(cvf, pmf, by=rt)
 rt2 <- get_shared_collections(cvpm, hpof)
 cvpmhp <- merge(cvpm, hpof, by=rt2[4,])
 
+searchTerm <- stringr::regex("Epilepsy", ignore_case=TRUE)
 scv <- cvf %>% 
    select(
       ClinVar_traits, ClinVar_traitNames, ClinVar_traitCref,
       ClinVar_entrezNames
    ) %>%
    filter(
-      ClinVar_traitNames=stringr::str_detect(name, "Epilepsy"),
+      ClinVar_traitNames=stringr::str_detect(name, searchTerm),
       ClinVar_entrezNames=symbol %in% c("SLC1A1", "SV2A")
    )
 spm <- pmf %>% 
    select(PubMed_medgenNames, PubMed_geneByPubmed) %>% 
    filter(
-      PubMed_medgenNames=stringr::str_detect(name, "Epilepsy"),
+      PubMed_medgenNames=stringr::str_detect(name, searchTerm),
       PubMed_geneByPubmed=entrez %in% c(6505, 9900)
    )
 scvpm <- merge(scv, spm)
 shp <- hpof %>% 
-   filter(HPO_diseases=stringr::str_detect(label, "Epilepsy"))
+   filter(HPO_diseases=stringr::str_detect(label, searchTerm))
 rt <- get_shared_collections(scvpm, shp)
 scvpmhp <- merge(scvpm, shp, by=rt[4,])
 plot(data_model(scvpmhp, rtOnly=TRUE, recursive=TRUE))
