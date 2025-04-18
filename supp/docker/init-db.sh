b3eaa576552cb21b7a134ffc0fd46dc639eb7a94
Patrice Godard <patrice.godard@angelinipharma.com>
2025-04-18 15:04:34 +0200

    Preparing ClickHouse upgrade

--- supp/docker/init-db.sh
+++ supp/docker/init-db.sh
@@ -8,31 +8,35 @@ if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     echo "-- First container startup --"
 
     cp /users-1.xml /etc/clickhouse-server/users.xml
+    cp /default-user-1.xml /etc/clickhouse-server/users.d/default-user.xml
     sleep 2
 
-    clickhouse client -n <<-EOSQL
-        CREATE TABLE default.System (name String, instance String, version String, contact String, path String) ENGINE = MergeTree() ORDER BY tuple();
-        CREATE TABLE default.Collections (title String, description String, json String) ENGINE = MergeTree() ORDER BY (title);
-        CREATE TABLE default.Users (login String, contact Nullable(String), admin UInt8, provider UInt8) ENGINE = MergeTree() ORDER BY (login);
-        INSERT INTO default.System (name, instance, version, contact, path) VALUES ('chTKCat', 'UCB - IT', '2022-12-04', 'Patrice Godard <patrice.godard@ucb.com>', '');
-        CREATE USER 'patrice.godard@ucb.com' IDENTIFIED WITH no_password;
-        GRANT ALL ON *.* TO 'patrice.godard@ucb.com' WITH GRANT OPTION;
-        INSERT INTO default.Users (login, admin, provider) VALUES ('patrice.godard@ucb.com', 1, 1);
-EOSQL
+    clickhouse client -q "CREATE TABLE default.System (name String, instance String, version String, contact String, path String) ENGINE = MergeTree() ORDER BY tuple();"
+    clickhouse client -q "CREATE TABLE default.Collections (title String, description String, json String) ENGINE = MergeTree() ORDER BY (title);"
+    clickhouse client -q "CREATE TABLE default.Users (login String, contact Nullable(String), admin UInt8, provider UInt8) ENGINE = MergeTree() ORDER BY (login);"
+    clickhouse client -q "INSERT INTO default.System (name, instance, version, contact, path) VALUES ('chTKCat', '${TKCAT_INSTANCE}', '${TKCAT_VERSION}', '${TKCAT_CONTACT}', '');"
+    clickhouse client -q "CREATE USER '${TKCAT_ADMIN}' IDENTIFIED WITH no_password;"
+    clickhouse client -q "GRANT ALL ON *.* TO '${TKCAT_ADMIN}' WITH GRANT OPTION;"
+    clickhouse client -q "INSERT INTO default.Users (login, admin, provider) VALUES ('${TKCAT_ADMIN}', 1, 1);"
+    clickhouse client -q "INSERT INTO default.Users (login, admin, provider) VALUES ('default', 0, 0) ;"
 
-    cp /users-2.xml /etc/clickhouse-server/users.xml
-    sleep 2
+   if [ -e "/etc/clickhouse-server/users.d/default-user.xml" ]; then
+      rm -rf "/etc/clickhouse-server/users.d/default-user.xml"
+   fi
+   cp /users-2.xml /etc/clickhouse-server/users.xml
+   cp /remove_default_user.yaml /etc/clickhouse-server/users.d/remove_default_user.yaml
+   sleep 2
 
-    clickhouse client -u patrice.godard@ucb.com -n <<-EOSQL
-        CREATE USER default IDENTIFIED WITH no_password;
-        INSERT INTO default.Users (login, admin, provider) VALUES ('default', 0, 0) ;
-        REVOKE ALL ON *.* FROM default;
-        GRANT SHOW DATABASES ON *.* TO default;
-        GRANT SHOW TABLES ON *.* TO default;
-        GRANT SHOW COLUMNS ON *.* TO default;
-        GRANT SELECT(name, instance, version, contact) ON default.System TO default;
-        GRANT SELECT ON default.Collections TO default;
-        GRANT SELECT(login, admin, provider) ON default.Users TO default;
+   clickhouse client -u ${TKCAT_ADMIN} -n <<-EOSQL
+       CREATE USER default IDENTIFIED WITH no_password;
+       REVOKE ALL ON *.* FROM default;
+       GRANT SHOW DATABASES ON *.* TO default;
+       GRANT SHOW TABLES ON *.* TO default;
+       GRANT SHOW COLUMNS ON *.* TO default;
+       GRANT SELECT(name, instance, version, contact) ON default.System TO default;
+       GRANT SELECT ON default.Collections TO default;
+       GRANT SELECT(login, admin, provider) ON default.Users TO default;
+       GRANT SELECT(value, name) ON system.build_options TO default;
 EOSQL
     
 else
--- supp/docker/launch-tkcat-instance.sh
+++ supp/docker/launch-tkcat-instance.sh
@@ -1,18 +1,40 @@
 #!/bins/bash
 
+##################################################################
+## Customize parameters
+IMAGE_BASE_NAME="test-tkcat"
+TKCAT_INSTANCE="test_tkcat"
+TKCAT_VERSION="2025.04.18"
+TKCAT_ADMIN="pgodard@domain.com"
+TKCAT_CONTACT="Admin User <pgodard@domain.com>"
+
+## Don't forget to create a password for the TKCAT_ADMIN user
+## In R: ?TKCat::change_chTKCat_password()
+
+TKCAT_NAT_PORT=9103 #9101
+TKCAT_HTTP_PORT=9113 #9111
+
+TKCAT_HOSTDIR=/mnt/data1/pgodard/Services-test
+
 ######################
-IMAGE=tkcat:2022.12.04
+## Automatic parameters
+IMAGE=${IMAGE_BASE_NAME}:${TKCAT_VERSION}
 
-docker build -t ${IMAGE} .
+CONTAINER=${TKCAT_INSTANCE}_${TKCAT_VERSION}
+TKCAT_HOME=${TKCAT_HOSTDIR}/${CONTAINER}
 
-######################
-CONTAINER=tkcat
-TKCAT_NAT_PORT=9000
-TKCAT_HTTP_PORT=8123
-
-TKCAT_HOME=/data/TKCat
 TKCAT_DATA=${TKCAT_HOME}/data
 TKCAT_LOG=${TKCAT_HOME}/log
+
+if [ -e "$TKCAT_HOME" ]; then
+    echo "File $TKCAT_HOME already exists. Exiting without doing anything."
+    exit 1
+fi
+
+##################################################################
+
+######################
+docker build -t ${IMAGE} .
 
 ######################
 mkdir -p ${TKCAT_HOME}
@@ -26,14 +48,13 @@ chmod a+rwx ${TKCAT_LOG}
 docker run -d \
 	--name ${CONTAINER} \
 	--ulimit nofile=262144:262144 \
-	--network=host \
+	--publish=${TKCAT_NAT_PORT}:9000 \
+   --publish=${TKCAT_HTTP_PORT}:8123 \
 	--volume ${TKCAT_DATA}:/var/lib/clickhouse \
 	--volume ${TKCAT_LOG}:/var/log/clickhouse-server \
+	-e TKCAT_INSTANCE="${TKCAT_INSTANCE}" \
+	-e TKCAT_VERSION="${TKCAT_VERSION}" \
+	-e TKCAT_ADMIN="${TKCAT_ADMIN}" \
+	-e TKCAT_CONTACT="${TKCAT_CONTACT}" \
 	--restart=always \
 	${IMAGE}
-
-	
-
-## Filtered by docker somehow ? ==> use of --network=host
-#	--publish=${TKCAT_NAT_PORT}:9000 \
-#	--publish=${TKCAT_HTTP_PORT}:8123 \
